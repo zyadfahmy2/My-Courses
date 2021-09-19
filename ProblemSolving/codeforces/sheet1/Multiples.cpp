3da43699c891b0a53952b15038ea59df94ae258c
ZiadFahmy <zyadfahmy2@gmail.com>
2021-09-20 01:42:25 +0200

    adding a few files to problemsolving

--- ProblemSolving/codeforces/sheet1/Multiples.cpp
+++ ProblemSolving/codeforces/sheet1/Multiples.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main(void)
 {
-    
+    int a, b;
+    cin >> a >> b;
+    //checking the a or b are divisible by each other
+    if(a%b == 0 || b%a == 0)
+    cout<< "Multiples" << "\n";
+    else
+    cout<< "No Multiples" << "\n";
 }
