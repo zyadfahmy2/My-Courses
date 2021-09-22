afd99c938ea59476a736b70bee5d7b9cfd3473fc
ZiadFahmy <zyadfahmy2@gmail.com>
2021-09-22 15:10:46 +0200

    new problems

---
+++ ProblemSolving/codeforces/sheet1/FixedPassword.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//#include <bits/stdc++.h>
+
+int main(void)
+{
+    int password = 0;
+    
+    while(password != 1999)
+    {
+        cin>>password;
+        cout<<"Wrong\n";
+    }
+    cout<<"Correct";
+}
